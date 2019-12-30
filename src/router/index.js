d2eae97024a33246e982dddc18f88892d5f89429
caidawei <caidawei@hikvision.com>
2019-12-30 16:42:31 +0800

    忽略token始终可登录

--- src/router/index.js
+++ src/router/index.js
@@ -45,10 +45,12 @@ router.beforeEach(async (to, from, next) => {
   if (to.matched.some(r => r.meta.auth)) {
     // 这里暂时将cookie里是否存有token作为验证是否登录的条件
     // 请根据自身业务需要修改
+    var flag = 'true'
     const token = util.cookies.get('token')
-    if (token === '' && token !== 'undefined') {
+    if (flag === 'true') {
       next()
     } else {
+      console.log(token)
       // 没有登录的时候跳转到登录界面
       // 携带上登陆成功之后需要跳转的页面完整路径
       next({
